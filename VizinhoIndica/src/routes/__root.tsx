0f166e41c1f4532a865c17c287aaf46d1bfb753c
Kayque Miqueias <kayquemiqueias17@gmail.com>
2026-07-04 23:27:40 -0300

    add web_icon

--- VizinhoIndica/src/routes/__root.tsx
+++ VizinhoIndica/src/routes/__root.tsx
@@ -97,7 +97,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: appCss,
       },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      { rel: "icon", href: "/web_icon.png", type: "image/x-icon" },
     ],
   }),
   shellComponent: RootShell,
